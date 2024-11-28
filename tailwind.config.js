501202c501be3ff4026b223d8a64fed755e4aa64
Holly Pepperony <3033310@gmail.com>
2024-11-28 15:54:13 +0700

    add tw color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        
+        //coolors
         // 'wa-text': '#140800',
         // 'wa-secondary-bg': '#efd595',
         // 'wa-border': '#264653',
@@ -18,11 +20,21 @@ module.exports = {
         // 'wa-bg-from': '#fdf4ec',
         // 'wa-bg-to': '#ffc592',
 
-        'wa-text': '#222222',
-        'wa-secondary-bg': '#F7F7F7',
-        'wa-border': '#F0F0F0',
-        'wa-button-bg': '#F0F0F0',
-        'wa-button-bg-hover': '#E0E0E0',
+        //duck style
+        // 'wa-text': '#222222',
+        // 'wa-secondary-bg': '#F7F7F7',
+        // 'wa-border': '#F0F0F0',
+        // 'wa-button-bg': '#F0F0F0',
+        // 'wa-button-bg-hover': '#E0E0E0',
+        // 'wa-bg-from': '#ffffff',
+        // 'wa-bg-to': '#ffffff',
+
+        //tailwind
+        'wa-text': '#364153',
+        'wa-secondary-bg': '#FAFBFD',
+        'wa-border': '#FAFBFD',
+        'wa-button-bg': '#4CA3E3',
+        'wa-button-bg-hover': '#3B83C3',
         'wa-bg-from': '#ffffff',
         'wa-bg-to': '#ffffff',
       },
