336d4ecddab0d3dd61d66d0f06ed617b6fdee03e
HollyPepperony <3033310@gmail.com>
2024-12-02 12:42:56 +0700

    Add manifest from Next.js website favicon generator

--- app/layout.js
+++ app/layout.js
@@ -28,8 +28,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#000000" />
+      <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="words" />
+<link rel="manifest" href="/site.webmanifest" />
         {/* Add other meta tags as needed */}
       </head>
       <body
